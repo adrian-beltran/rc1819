0347422b01f7c21433fd19914ec6bedaaf8c12c5
unknown <adrian.beltran@alu.uhu.es>
2019-03-19 14:00:23 +0100

    concatenar

--- 3-enesimo.pl
+++ 3-enesimo.pl
@@ -2,4 +2,4 @@
 % Es cierto cuando Elem unifica con el elemento que ocupa la posición Pos de la Lista
 
 elemento_enesimo(1, [Cab|_], Cab).
-elemento_enesimo(N, [_|R], E):- elemento_enesimo(N2, R, E), N2 is N - 1.
+elemento_enesimo(N, [_|R], E):- elemento_enesimo(N2, R, E), N is N2+1.
