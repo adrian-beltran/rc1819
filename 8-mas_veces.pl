fe68eb677d6273e520afe23f9391200a3ec65060
unknown <adrian.beltran@alu.uhu.es>
2019-04-30 14:22:54 +0200

    Fin listas

--- 8-mas_veces.pl
+++ 8-mas_veces.pl
@@ -3,8 +3,24 @@
 % es cierto cuando Elem unifica con el elemento que se repite más veces
 % en la lista Lista y Num unifica con el número de veces que se repite dicho elemento
 
-mas_veces([], _, 0).
+mas_veces(L, E, N) :-
+	msort(L,L2),
+	comprime(L2,R),
+	mayor(R, (E,N)).
+	
+% comprime(+Lista, -Resultado)
+% es cierto cuando Resultado unifica con una lista en el siguiente fomrato:
+% 	comprime(([1,1,1,2,2,3,4,4,4], R).
+% 	R= [(1,3), (2,2), (3,1), (4,3)]
 
-mas_veces([Cab|Resto], Cab, N) :-
-	mas_veces(Resto, Cab, N2),
-	N is N2 + 1.
+comprime([], []).
+comprime([E], [(E,1)]).
+comprime([Cab, Cab|Resto], [(E,N2)|R]):- comprime([Cab|Resto], [(E,N)|R]), N2 is N + 1.
+comprime([Cab1, Cab2|Resto], [(Cab1,1)|R]):- Cab1 \= Cab2, comprime([Cab2|Resto], R).
+
+% mayor(+Lista, -Tupla)
+% es cierto si Tupla unifica con una tupla (E,N) de mayor N de la lista Lista
+
+mayor([(E,N)],(E,N)).
+mayor([(E,N)|Resto], (E,N)):- mayor(Resto, (_,N1)), N > N1.
+mayor([(_,N)|Resto], (E1,N1)):- mayor(Resto, (E1,N1)), N1 >= N.
